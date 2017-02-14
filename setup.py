09157cb5f59ad8e67393d153b61045b0e2f10eb1
alvations <alvations@gmail.com>
2017-02-14 16:26:36 +0800

    fixed missing print_function in wiki

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 
 setup(
     name='lazyme',
-    version='0.0.4',
+    version='0.0.6',
     packages=['lazyme',],
     description='Lazy python recipes',
     license="MIT",
