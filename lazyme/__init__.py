b7a40ae52e5793f0d835465f55e68ddc6316eb48
alvations <alvations@gmail.com>
2017-12-21 13:55:49 +0800

    added find_file to top import

--- lazyme/__init__.py
+++ lazyme/__init__.py
@@ -1,4 +1,9 @@
 
+from lazyme.fileio import find_file
+
 from lazyme.iterate import per_section, per_chunk, per_window
-from lazyme.string import deduplicate
+from lazyme.iterate import zigzag
+
+from lazyme.string import *
+
 from lazyme.wikipedia import iter_paragraph as iter_wiki
