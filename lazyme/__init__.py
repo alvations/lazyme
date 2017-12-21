45c5bc785f861a81f3828001f9d0294608fbafc6
alvations <alvations@gmail.com>
2017-12-21 14:02:20 +0800

    clean up top level import

--- lazyme/__init__.py
+++ lazyme/__init__.py
@@ -4,6 +4,8 @@ from lazyme.fileio import find_file
 from lazyme.iterate import per_section, per_chunk, per_window
 from lazyme.iterate import zigzag
 
-from lazyme.string import *
+from lazyme.string import color_print, color_str
+from lazyme.string import deduplicate
+from lazyme.string import remove_text_inside_brackets, remove_html_tags
 
 from lazyme.wikipedia import iter_paragraph as iter_wiki
