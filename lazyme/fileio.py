7475bc96a95e78fa235833482c33fe8ee2345b7c
alvations <alvations@gmail.com>
2017-12-21 13:53:49 +0800

    added find file recursively

---
+++ lazyme/fileio.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import fnmatch
+import glob
+import os
+import sys
+
+
+def find_files(dir_path, extension="*"):
+    """
+    From https://stackoverflow.com/a/2186565/610569
+    """
+    if sys.version_info.major == 3:
+        pattern = '/'.join([dir_path, '**', extension])
+        if sys.version_info.minor >= 5:
+            for filename in glob.iglob(pattern, recursive=True):
+                yield filename
+    for root, dirnames, filenames in os.walk(dir_path):
+        for filename in fnmatch.filter(filenames, extension):
+            yield os.path.join(root, filename)
