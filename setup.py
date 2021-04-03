843fafac9e7e7f4191818f2f1f03b203d460977b
alvations <alations@gmail.com>
2021-04-03 11:50:12 -0400

    up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 
 setup(
     name='lazyme',
-    version='0.0.23',
+    version='0.0.24',
     packages=['lazyme',],
     url = 'https://github.com/alvations/lazyme',
     description='Lazy python recipes',
